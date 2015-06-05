495b36afe02df64bfd10a5c65e3302ef07b27a2c
Dan Slov <dan.slov@gmail.com>
2015-06-05 07:52:53 -0700

    merging

---
+++ dot_files/git-diff-wrapper.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# git-diff-wrapper.sh
+# diff is called by git with 7 parameters:
+# path old-file old-hex old-mode new-file new-hex new-mode
+# git config --global diff.external ~/git-diff-wrapper.sh 
+/Applications/homebrew/bin/mvimdiff "$2" "$5" | cat
