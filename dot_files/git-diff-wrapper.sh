6cfd6923d9dd1576d7543168519a4542b1e7026a
Dan Slov <dan.slov@gmail.com>
2015-06-05 07:49:48 -0700

    updated diff tool

---
+++ dot_files/git-diff-wrapper.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# git-diff-wrapper.sh
+# diff is called by git with 7 parameters:
+# path old-file old-hex old-mode new-file new-hex new-mode
+# git config --global diff.external <path_to_this_script>
+/Applications/homebrew/bin/mvimdiff "$2" "$5" | cat
+
+
+# example of use:
+# git diff lebigmac master  -- vimwiki/index.wiki
