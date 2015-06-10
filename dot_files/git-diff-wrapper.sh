d65aa950f33f752fdd874e7e48ed9a198429a48a
Dan Slov <dan.slov@gmail.com>
2015-06-10 09:26:42 -0700

    premerge

--- dot_files/git-diff-wrapper.sh
+++ dot_files/git-diff-wrapper.sh
@@ -4,3 +4,7 @@
 # path old-file old-hex old-mode new-file new-hex new-mode
 # git config --global diff.external ~/git-diff-wrapper.sh 
 /Applications/homebrew/bin/mvimdiff "$2" "$5" | cat
+
+
+# example of use:
+# git diff lebigmac master  -- vimwiki/index.wiki
